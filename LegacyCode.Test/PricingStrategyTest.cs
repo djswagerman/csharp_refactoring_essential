d80651dd4db1d23a43be2f72548979a56f593680
Dirk-Jan Swagerman <dirkjan@buffadoo.nl>
2026-06-22 16:25:52 +0200

    refactor: consolidate pricing strategy tests into parameterized test

--- LegacyCode.Test/PricingStrategyTest.cs
+++ LegacyCode.Test/PricingStrategyTest.cs
@@ -2,58 +2,17 @@ namespace LegacyCode.Test;
 
 public class PricingStrategyTest
 {
-    private readonly double _weight;
-    private readonly double _distance;
-    private readonly IPricingStrategy standardPricing;
-    private readonly IPricingStrategy expressPricing;
-    private readonly IPricingStrategy overnightPricing;
-    private readonly IPricingStrategy internationalPricing;
-
-    public PricingStrategyTest()
+    [Theory]
+    [InlineData("STANDARD", 5, 120, 5 * 0.5)]
+    [InlineData("EXPRESS", 5, 120, 5 * 0.8 + 120 * 0.1)]
+    [InlineData("OVERNIGHT", 5, 120, 5 * 1.2 + 25)]
+    [InlineData("INTERNATIONAL", 5, 120, 5 * 1.5)]
+    public void ShippingCalculator_PricingStrategy(
+        string shippingType, double weight, double distance, double expectedPricing)
     {
-        standardPricing = PricingStrategyFactory.CreateForShippingType("STANDARD");
-        expressPricing = PricingStrategyFactory.CreateForShippingType("EXPRESS");
-        overnightPricing = PricingStrategyFactory.CreateForShippingType("OVERNIGHT");
-        internationalPricing = PricingStrategyFactory.CreateForShippingType("INTERNATIONAL");
+        var pricing = PricingStrategyFactory.CreateForShippingType(shippingType);
+        var calculatedPricing = pricing.CalculatePrice(weight, distance);
 
-        _weight = 5;
-        _distance = 120;
-    }
-    
-    [Fact]
-    public void ShippingCalculator_StandardPricingStrategy()
-    {
-        var expectedPricing = _weight * 0.5;
-        var calculatedPricing = standardPricing.CalculatePrice(_weight, _distance);
-        
-        Assert.Equal(expectedPricing, calculatedPricing);
-    }
-
-    [Fact]
-    public void ShippingCalculator_ExpressPricingStrategy()
-    {
-        var expectedPricing = _weight * 0.8 + _distance * 0.1;
-        var calculatedPricing = expressPricing.CalculatePrice(_weight, _distance);
-        
-        Assert.Equal(expectedPricing, calculatedPricing);
-    }
-
-    [Fact]
-    public void ShippingCalculator_OvernightPricingStrategy()
-    {
-        var expectedPricing = _weight * 1.2 + 25;
-        var calculatedPricing = overnightPricing.CalculatePrice(_weight, _distance);
-        
-        Assert.Equal(expectedPricing, calculatedPricing);
-        
-    }
-
-    [Fact]
-    public void ShippingCalculator_InternationalPricingStrategy()
-    {
-        var expectedPricing = _weight * 1.5;
-        var calculatedPricing = internationalPricing.CalculatePrice(_weight, _distance);
-        
         Assert.Equal(expectedPricing, calculatedPricing);
     }
 }

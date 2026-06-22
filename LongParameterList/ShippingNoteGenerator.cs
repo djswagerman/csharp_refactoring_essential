5559aa07ef70b0f0caf54b27550bf6ab830165aa
Dirk-Jan Swagerman <dirkjan@buffadoo.nl>
2026-06-22 14:03:07 +0200

    extract Address class from Customer to reduce LongParameterList

--- LongParameterList/ShippingNoteGenerator.cs
+++ LongParameterList/ShippingNoteGenerator.cs
@@ -1,14 +1,41 @@
 ﻿namespace LongParameterList;
 
-public class Customer(string customerFirstName, string customerLastName, string addressLine1, string addressLine2, string city, string postcode, string country)
+public class Address
 {
-    public string CustomerFirstName { get; } = customerFirstName;
-    public string CustomerLastName { get; } = customerLastName;
-    public string AddressLine1 { get; } = addressLine1;
-    public string AddressLine2 { get; } = addressLine2;
-    public string City { get; } = city;
-    public string Postcode { get; } = postcode;
-    public string Country { get; } = country;
+    public Address(string addressLine1, string addressLine2, string city, string postcode, string country)
+    {
+        AddressLine1 = addressLine1;
+        AddressLine2 = addressLine2;
+        City = city;
+        Postcode = postcode;
+        Country = country;
+    }
+
+    public string AddressLine1 { get; }
+    public string AddressLine2 { get; }
+    public string City { get; }
+    public string Postcode { get; }
+    public string Country { get; }
+}
+
+public class Customer
+{
+    private readonly Address _address;
+
+    public Customer(string customerFirstName, string customerLastName, string addressLine1, string addressLine2, string city, string postcode, string country)
+    {
+        CustomerFirstName = customerFirstName;
+        CustomerLastName = customerLastName;
+        _address = new Address(addressLine1, addressLine2, city, postcode, country);
+    }
+
+    public string CustomerFirstName { get; }
+    public string CustomerLastName { get; }
+
+    public Address Address
+    {
+        get { return _address; }
+    }
 }
 
 public class Order(string orderId, string itemDescription, int quantity)
@@ -28,11 +55,11 @@ public class ShippingNoteGenerator
         var quantity = order.Quantity;
         var customerFirstName = customer.CustomerFirstName;
         var customerLastName = customer.CustomerLastName;
-        var addressLine1 = customer.AddressLine1;
-        var addressLine2 = customer.AddressLine2;
-        var city = customer.City;
-        var postcode = customer.Postcode;
-        var country = customer.Country;
+        var addressLine1 = customer.Address.AddressLine1;
+        var addressLine2 = customer.Address.AddressLine2;
+        var city = customer.Address.City;
+        var postcode = customer.Address.Postcode;
+        var country = customer.Address.Country;
         string fullName = customerFirstName + " " + customerLastName;
 
         string address = addressLine1 + ", "
